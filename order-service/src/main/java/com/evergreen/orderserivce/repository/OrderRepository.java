1f24d34dbcc28150fe3e4a5cc7759dc6e4a1eb57
michaelbaquet <129547502+michaelbaquet@users.noreply.github.com>
2023-08-13 14:31:50 -0600

    Delete order-service directory

--- order-service/src/main/java/com/evergreen/orderserivce/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.evergreen.orderserivce.repository;
-
-import com.evergreen.orderserivce.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
